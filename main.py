4dd164ae723c1f4f7704af341abe528a4ef7df8a
william1616 <william1616@users.noreply.github.com>
2014-09-27 20:47:14 +0100

    update

--- main.py
+++ main.py
@@ -1,15 +1,11 @@
 from time import sleep
-from transmitReceive import TransmitReceive
-from gps import GPS
+#from transmitReceive import TransmitReceive
+from Camera import camera
+#from gps import GPS
 from AccGyro import accGyro
 from TempPressureSensor import tempPressure
 
 #Setup Sensors
-#GPS
-myGPS = GPS()
-myGPS.createFile()
-myGPS.startRead()
-
 #AccGyro
 myAccGyro = accGyro()
 myAccGyro.createFile()
@@ -18,28 +14,16 @@ myAccGyro.createFile()
 myTempPressure = tempPressure()
 myTempPressure.createFile()
 
-#TransmitReceive
-myTransmitReceive = TransmitReceive()
-
 #Loop
-while True:
-  #Record Data
-  myGPS.writeData()
-  print "lat:", myGPS.lat, "lng:", myGPS.lng
-  myAccGyro.getResults()
-  myAccGyro.writeData()
-  print 'Acceleration x:', myAccGyro.acc_x, 'Acceleration y:', myAccGyro.acc_y, 'Acceleration z:', myAccGyro.acc_z, 'Temperature:', myAccGyro.temp, 'Gyroscope x:', myAccGyro.gyro_x, 'Gyroscope y:', myAccGyro.gyro_y, 'Gyroscope z:', myAccGyro.gyro_z
-  myTempPressure.getResults()
-  myTempPressure.writeData()
-  print "Temperature:", myTempPressure.temp, "Pressure:", myTempPressure.pressure
-  
-  #Broadcast Data
-  transmitString = "$" + str(myGPS.lat) + "*" + str(myGPS.lng) + "&"
-  print transmitString
-  myTransmitReceive.sendData([ord(char) for char in transmitString])
-  
-  #Sleep
-  sleep(1)
-
-#End
-myGPS.stopRead()
+with camera() as cam:
+  while True:
+    #Record Data
+    print "lat:", myGPS.lat, "lng:", myGPS.lng
+    myAccGyro.getResults()
+    myAccGyro.writeData()
+    print 'Acceleration x:', myAccGyro.acc_x, 'Acceleration y:', myAccGyro.acc_y, 'Acceleration z:', myAccGyro.acc_z, 'Temperature:', myAccGyro.temp, 'Gyroscope x:', myAccGyro.gyro_x, 'Gyroscope y:', myAccGyro.gyro_y, 'Gyroscope z:', myAccGyro.gyro_z
+    myTempPressure.getResults()
+    myTempPressure.writeData()
+    print "Temperature:", myTempPressure.temp, "Pressure:", myTempPressure.pressure
+    
+    cam.capture()
