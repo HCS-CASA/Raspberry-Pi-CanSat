d2517d95c6741e2a239d5a852c7e9289c8af4af8
william1616 <william1616@users.noreply.github.com>
2014-09-27 20:38:29 +0100

    update

--- Camera.py
+++ Camera.py
@@ -4,7 +4,7 @@ from time import sleep
 class camera(picamera.PiCamera):
   def __init__(self):
     super(camera, self).__init__()
-    self.imgGen = super(camera, self).capture_continuous(ps.path.join(os.path.dirname(os.path.realpath(__file__)),"img/image{counter:04d}.jpg"))
+    self.imgGen = super(camera, self).capture_continuous(os.path.join(os.path.dirname(os.path.realpath(__file__)),"img/image{counter:04d}.jpg"))
     self.resolution = (2592, 1944) #2582x1944 = Max Resoloution
     self.brightness = 50 #0 -> 100
     self.contrast = 0 #-100 -> 100
